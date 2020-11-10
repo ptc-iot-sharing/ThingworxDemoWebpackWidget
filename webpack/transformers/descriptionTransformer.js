b113b7daad289d2880cbb007e58f45b4437ab8dd
BogdanMihaiciuc <bogdanmihaiciuc@gmail.com>
2020-11-10 11:50:01 +0200

    Add method to check for existence of decorator

--- webpack/transformers/descriptionTransformer.js
+++ webpack/transformers/descriptionTransformer.js
@@ -15,4 +15,38 @@
  */
 class DescriptionTransformer {
 
+
+
+    /**
+     * Checks whether the given node has a decorator or decorator factory with the given name.
+     * @param name      The name of the decorator to find.
+     * @param node      The node in which to search.
+     * @return          `true` if the decorator was found, `false` otherwise.
+     */
+    hasDecoratorNamed(name, node) {
+        if (!node.decorators) return false;
+
+        // Getting the decorator name depends on whether the decorator is applied directly or via a
+        // decorator factory.
+        for (const decorator of node.decorators) {
+            
+            // In a decorator factory, the decorator itself is the result of invoking
+            // the decorator factory function so it doesn't technically have a name; in this case the name
+            // of the decorator factory function is considered to be the decorator name.
+            if (decorator.expression.kind == ts.SyntaxKind.CallExpression) {
+                const callExpression = decorator.expression;
+                if (callExpression.expression.getText() == name) {
+                    return true;
+                }
+            }
+            else if (decorator.expression.kind == ts.SyntaxKind.Identifier) {
+                const identifierExpression = decorator.expression;
+                if (identifierExpression.text == name) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
